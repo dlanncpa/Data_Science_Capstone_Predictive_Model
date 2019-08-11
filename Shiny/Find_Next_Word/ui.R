149f73818f0dcb363f010ac8466d8877665d987c
dlanncpa <drlanning1976@hotmail.com>
2019-08-11 14:47:37 -0400

    Added explanatory text to the UI.

--- Shiny/Find_Next_Word/ui.R
+++ Shiny/Find_Next_Word/ui.R
@@ -14,5 +14,9 @@ shinyUI(fluidPage(
         actionButton("button2", label = textOutput("next_word2")),
         actionButton("button3", label = textOutput("next_word3"))
         )
-    )
+    ),
+    p("This app predicts the next word of the text typed in the text box. Three available next words
+      are produced on three buttons below the text box. If one of the buttons contain the next word you
+      would like to use, click the button and the word will be added to the end of the text in the text
+      box.")
 ))

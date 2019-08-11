e423f9f89c7a82d89437be6bdecf312eb273f8c7
dlanncpa <drlanning1976@hotmail.com>
2019-08-11 14:46:54 -0400

    Removed the if==0 from the if statements. Adjusted the unigram samples.

--- Shiny/Find_Next_Word/find_next_word.r
+++ Shiny/Find_Next_Word/find_next_word.r
@@ -25,7 +25,7 @@ find_next_word<-function(x){
                 & quintgram$w3 == inputAdj[3] & quintgram$w4 == inputAdj[4], 5], 3), n = 1)
         return(c(a1, a2, a3))
     }
-        else if(length(find_next_word)==0 || length(inputAdj)==3){
+        else if(length(inputAdj)==3){
             inputAdj<-tail(inputAdj, 3)
             a1<-head(quadgram[quadgram$w1 == inputAdj[1] & quadgram$w2 == inputAdj[2]
                     & quadgram$w3 == inputAdj[3], 4], 1)
@@ -35,14 +35,14 @@ find_next_word<-function(x){
                     & quadgram$w3 == inputAdj[3], 4], 3), n = 1)
             return(c(a1, a2, a3))
         }
-            else if(length(find_next_word)==0 || length(inputAdj)==2){
+            else if(length(inputAdj)==2){
                 inputAdj<-tail(inputAdj, 2)
                 a1<-head(trigram[trigram$w1 == inputAdj[1] & trigram$w2 == inputAdj[2], 3], 1)
                 a2<-tail(head(trigram[trigram$w1 == inputAdj[1] & trigram$w2 == inputAdj[2], 3], 2), n = 1)
                 a3<-tail(head(trigram[trigram$w1 == inputAdj[1] & trigram$w2 == inputAdj[2], 3], 3), n = 1)
                 return(c(a1, a2, a3))
             }
-                else if(length(find_next_word)==0 || length(inputAdj)==1){
+                else if(length(inputAdj)==1){
                     inputAdj<-tail(inputAdj, 2)
                     a1<-head(bigram[bigram$w1 == inputAdj[1], 2], 1)
                     a2<-tail(head(bigram[bigram$w1 == inputAdj[1], 2], 2), n = 1)
@@ -50,15 +50,15 @@ find_next_word<-function(x){
                     return(c(a1, a2, a3))
                 }
                     else if(length(find_next_word)==0){
-                        a1<-sample(unigram[, 1])
-                        a2<-sample(unigram[, 1])
-                        a3<-sample(unigram[, 1])
+                        a1<-sample(unigram[1:50, 1])
+                        a2<-sample(unigram[1:50, 1])
+                        a3<-sample(unigram[1:50, 1])
                         return(c(a1, a2, a3))
                     }
     else{
-        a1<-sample(unigram[, 1])
-        a2<-sample(unigram[, 1])
-        a3<-sample(unigram[, 1])
+        a1<-sample(unigram[1:50, 1])
+        a2<-sample(unigram[1:50, 1])
+        a3<-sample(unigram[1:50, 1])
         return(c(a1, a2, a3))
     }
 }
